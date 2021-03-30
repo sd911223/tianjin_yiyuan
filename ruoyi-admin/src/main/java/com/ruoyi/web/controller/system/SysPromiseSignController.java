e18c46994c60637988a6e5bb9d4d2fbfc229680e
sd911223 <460475302@qq.com>
2021-03-30 10:43:47 +0800

    BUG

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/SysPromiseSignController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/SysPromiseSignController.java
@@ -8,6 +8,7 @@ import com.ruoyi.common.core.page.TableDataInfo;
 import com.ruoyi.common.enums.BusinessType;
 import com.ruoyi.common.exception.BaseException;
 import com.ruoyi.common.utils.JsonLoop;
+import com.ruoyi.common.utils.StringUtils;
 import com.ruoyi.common.utils.poi.ExcelUtil;
 import com.ruoyi.system.domain.SysPromiseSign;
 import com.ruoyi.system.service.ISysPromiseSignService;
@@ -43,8 +44,14 @@ public class SysPromiseSignController extends BaseController {
      */
     @ApiOperation("承诺详细列表")
     @GetMapping("/list")
-    public TableDataInfo list(SysPromiseSign sysPromiseSign) {
+    public TableDataInfo list(@RequestParam("pageNum") Integer pageNum,
+                              @RequestParam("pageSize") Integer pageSize,
+                              @RequestParam(value = "basicInfo",required = false) String basicInfo) {
         startPage();
+        SysPromiseSign sysPromiseSign =new SysPromiseSign();
+        if (!StringUtils.isEmpty(basicInfo)){
+            sysPromiseSign.setBasicInfo(basicInfo);
+        }
         List<SysPromiseSign> list = sysPromiseSignService.selectSysPromiseSignList(sysPromiseSign);
         if (!list.isEmpty()) {
             list.forEach(e -> {
